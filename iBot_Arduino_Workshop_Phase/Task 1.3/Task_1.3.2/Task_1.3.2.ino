081676eb59265c5da89f442ea6fc137534519154
nikhilarun17 <nikhil.arun@icloud.com>
2026-02-08 11:45:39 +0530

    Update Task_1.3.2.ino

--- iBot_Arduino_Workshop_Phase/Task 1.3/Task_1.3.2/Task_1.3.2.ino
+++ iBot_Arduino_Workshop_Phase/Task 1.3/Task_1.3.2/Task_1.3.2.ino
@@ -1,3 +1,11 @@
+/*
+Developer   - Nikhil Arun
+Date        - 7-2-2026
+Board       - Arduino Uno R3
+*/
+
+// And OLED Snake-type game with a single pixel.
+
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
