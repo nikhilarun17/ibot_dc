9366ec26087e56952bc6ffdd1a9cc60a91852fc8
nikhilarun17 <nikhil.arun@icloud.com>
2026-02-08 11:35:19 +0530

    Update Task_1.2.1.ino

--- iBot_Arduino_Workshop_Phase/Task 1.2/Task_1.2.1/Task_1.2.1.ino
+++ iBot_Arduino_Workshop_Phase/Task 1.2/Task_1.2.1/Task_1.2.1.ino
@@ -1,3 +1,11 @@
+/*
+Developer   - Nikhil Arun
+Date        - 6-2-2026
+Board       - Arduino Uno R3
+*/
+
+//Makes the buzzer buzz with a certain frequency every 1 sec
+
 int buzzerpin = 9;
 void setup() {
   pinMode(buzzerpin, OUTPUT);
